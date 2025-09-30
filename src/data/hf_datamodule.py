cbb7a4baae4f6afc53fdbfff2a76317775ed46f1
MakArtKar <makoyan2001@gmail.com>
2025-09-30 15:51:41 +0200

    feat: added HFDataModule

---
+++ src/data/hf_datamodule.py
@@ -0,0 +1,82 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, random_split
+from datasets import DictDataset, Dataset
+
+
+class HFDataModule(LightningDataModule):
+    def __init__(
+        self,
+        hf_dict_dataset: DictDataset,
+        val_ratio: float = 0.1,
+        dataloader_kwargs: dict[str, Any] = {},
+    ) -> None:
+        """Initialize a `HFDataModule`.
+
+        :param hf_dict_dataset: The Hugging Face DictDataset.
+        :param val_ratio: The ratio of the validation set. Defaults to `0.1`.
+        :param dataloader_kwargs: The keyword arguments for the dataloader. Defaults to `{}`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.hf_dict_dataset = hf_dict_dataset
+        self.val_ratio = val_ratio
+        self.dataloader_kwargs = dataloader_kwargs
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            batch_size = self.dataloader_kwargs.get("batch_size", 1)
+            if batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.dataloader_kwargs["batch_size"] = batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            self.data_test = self.hf_dict_dataset["test"]
+            self.data_train, self.data_val = self.hf_dict_dataset["train"].train_test_split(self.val_ratio, seed=42).values()
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            **self.dataloader_kwargs,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            **self.dataloader_kwargs,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            **self.dataloader_kwargs,
+            shuffle=False,
+        )
