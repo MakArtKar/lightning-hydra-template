dd0acb26f9a3dfb766df1a541796ede7f68458c9
MakArtKar <makoyan2001@gmail.com>
2025-10-20 11:23:48 +0200

    feat: added base and image transforms

---
+++ ml_core/transforms/base.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable, Mapping
+
+import torch.nn as nn
+
+class RenameTransform(nn.Module):
+    def __init__(self, mapping: Mapping[str, str]) -> None:
+        super().__init__()
+        self.mapping = mapping
+
+    def __call__(self, batch: Mapping[str, Any]) -> Mapping[str, Any]:
+        return {new_key: batch[old_key] for old_key, new_key in self.mapping.items()}
+
+
+class ComposeTransform(nn.Module):
+    def __init__(self, **transforms) -> None:
+        super().__init__()
+        self.transforms = nn.ModuleDict(transforms)
+
+    def __call__(self, batch: Mapping[str, Any]) -> Mapping[str, Any]:
+        for transform in self.transforms.values():
+            batch = batch | transform(batch)
+        return batch
+
+
+class WrapTransform(nn.Module):
+    def __init__(self, transform: Callable, new_key: str, mapping: Mapping[str, str] | None = None):
+        super().__init__()
+        self.transform = transform
+        self.new_key = new_key
+        self.mapping = mapping
+
+    def __call__(self, batch: Mapping[str, Any]) -> Mapping[str, Any]:
+        if self.mapping is not None:
+            input_batch = {new_key: batch[old_key] for old_key, new_key in self.mapping.items()}
+        else:
+            input_batch = batch
+        output = self.transform(**input_batch)
+        return batch | {self.new_key: output}
